f714a15a3fa5b1931aaf1eb2de7e05ddeebb2af1
LinderJunior <linderju01@gmail.com>
2022-01-25 15:35:30 +0200

    MODULO II aula 03 - Repositorio Cliente

--- algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
+++ algalog-api/src/main/java/com/algaworks/algalog/api/controller/ClienteController.java
@@ -6,22 +6,26 @@ import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algalog.domain.model.Cliente;
+import com.algaworks.algalog.domain.repositoy.ClienteRepository;
 
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 @RestController
 public class ClienteController {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
 	
-	@PersistenceContext
-	private EntityManager manager;
 	
 	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-
-		return manager.createQuery("from Cliente", Cliente.class)
-				.getResultList();
+		return clienteRepository.findByNome("Linder");
 	}
 }
