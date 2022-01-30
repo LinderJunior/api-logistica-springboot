51ccf0f7deaea66682c8551bf2aef684e92aa9cb
LinderJunior <linderju01@gmail.com>
2022-01-31 00:30:24 +0200

    Modulo 3.5 - Simplificando a transformação de objetos com ModelMapper

---
+++ algalog-api/src/main/java/com/algaworks/algalog/api/model/ClienteResummoModel.java
@@ -0,0 +1,19 @@
+package com.algaworks.algalog.api.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter 
+public class ClienteResummoModel {
+	
+	
+	private Long id;
+	private String nome;
+	
+	
+	
+	
+	
+
+}
